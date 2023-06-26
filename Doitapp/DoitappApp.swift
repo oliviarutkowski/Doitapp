373abb71acf7eb3e3305cedf41476042c3f61725
Olivia Rutkowski <oliviarutkowski08@gmail.com>
2023-06-26 15:39:23 -0600

    Initial Commit

---
+++ Doitapp/DoitappApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoitappApp.swift
+//  Doitapp
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct DoitappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
